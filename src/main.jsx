7a9cdf5ef5288cd7b4ad5b1cfb5b0d0beff58679
appleflyer <164886846+appleflyerv3@users.noreply.github.com>
2024-10-23 21:53:49 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import "dreamland";
 import { Route, Router } from "dreamland-router";
 import Home from "./routes/home";
-import Privacy from "./routes/privacy";
-import Terms from "./routes/terms";
 import Error from "./routes/error";
 import "./index.css";
 
